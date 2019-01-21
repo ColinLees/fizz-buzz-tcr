eeabe19eb1a716fcf1e4d6995c37b0e3e8a5e6fe
Colin Lees <colin.lees@stockport.gov.uk>
2019-01-21 09:32:03 +0000

    Test Commit Revert

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports.single = (n) => {
-  if (n % 3 === 0 && n % 5 === 0) { return 'FizzBuzz' }
-  if (n % 3 === 0) { return 'Fizz' }
-  if (n % 5 === 0) { return 'Buzz' }
+  if (isFizz(n) && isBuzz(n)) { return 'FizzBuzz' }
+  if (isFizz(n)) { return 'Fizz' }
+  if (isBuzz(n)) { return 'Buzz' }
   return n.toString()
 }
+
+const isFizz = (n) => n % 3 === 0
+const isBuzz = (n) => n % 5 === 0
