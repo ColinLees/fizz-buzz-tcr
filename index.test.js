edc62298e2c557f504f7ea68649c448b9fa5e455
Colin Lees <colin.lees@stockport.gov.uk>
2019-01-21 09:19:19 +0000

    Test Commit Revert

--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
 /* global describe, it */
 const assert = require('chai').assert
+const fizzBuzzer = require('.')
 
-describe('Testing', () => {
-  it('should work', () => {
-    assert.isTrue(true)
+describe('FizzBuzz', () => {
+  it('should return "1" for 1', () => {
+    const result = fizzBuzzer.single(1)
+    assert.equal(result, '1')
   })
 })
