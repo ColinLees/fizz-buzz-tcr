e945b53b264cf1736a7e238f3a3120c65ae9a0ae
Jordan Burns <jordan.burns@stockport.gov.uk>
2019-03-27 15:09:32 +0000

    Test Commit Revert

--- index.test.js
+++ index.test.js
@@ -7,4 +7,8 @@ describe('Testing', () => {
         assert.equal(app.displayFizzBuzz(15), 15);
         assert.equal(app.displayFizzBuzz(10), 10);
     })
+
+    it('should return fizz when parameter is a multiple of 3', () => {
+        assert.isTrue(true);
+    })
 })
