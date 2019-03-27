dea866c7469b8fa35247a5637e4b66deeedfa71b
Josh Hiles <joshhiles1@live.co.uk>
2019-03-27 15:01:44 +0000

    Test Commit Revert

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+function fizzBuzzMethod(input) {
+    return input
+}
 
+module.exports = {
+    fizzBuzzMethod
+}
--- index.test.js
+++ index.test.js
@@ -1,8 +1,13 @@
 /* global describe, it */
 const assert = require('chai').assert
+const fizzBuzz = require('.')
 
 describe('Testing', () => {
     it('should work', () => {
         assert.isTrue(true)
+    })
+
+    it('should return number', () => {
+        assert.equal(fizzBuzz.fizzBuzzMethod(2), 2)
     })
 })
