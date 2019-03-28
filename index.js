f30840844765083c27868704bf9ebab567bf6a88
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-03-28 08:27:24 +0000

    Test Commit Revert

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const fizzbuzz = (num) => {
+	if(num % 3 == 0 && num % 5 == 0) {
+		return 'fizzbuzz'
+	}
+	if(num % 3 == 0) {
+		return 'fizz'
+	}
+	if(num % 5 == 0) {
+		return 'buzz'
+	}
+	return num.toString()
+}
 
+const runFizzbuzz = (limit) => {
+	const list = []
+	for (let index = 1; index <= limit; index++) {
+		let result = fizzbuzz(index)
+		list.push(result)	
+	}
+	console.log(list)
+	return list
+}
+
+module.exports = { fizzbuzz, runFizzbuzz }
--- index.test.js
+++ index.test.js
@@ -1,8 +1,33 @@
 /* global describe, it */
 const assert = require('chai').assert
+const fizzbuzz = require('./index')
 
 describe('Testing', () => {
-    it('should work', () => {
-        assert.isTrue(true)
+    it('should return 1', () => {
+        const result = fizzbuzz.fizzbuzz(1)
+        assert.equal(result, '1')
+    })
+
+    it('should return fizz', () => {
+        const result = fizzbuzz.fizzbuzz(3)
+        assert.equal(result, 'fizz')
+    })
+
+    it('should return buzz', () => {
+        const result = fizzbuzz.fizzbuzz(5)
+        assert.equal(result, 'buzz')
+    })
+
+    it('should return fizzbuzz', () => {
+        const result = fizzbuzz.fizzbuzz(15)
+        assert.equal(result, 'fizzbuzz')
     })
 })
+
+// describe('runFizzbuzz', () => {
+//     it('should return correct results for numbers up to 15', () => {
+//         const expectedResult = ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz']
+//         const result = fizzbuzz.runFizzbuzz(15)
+//         assert.notDeepEqual(result, expectedResult)
+//     })
+// })
