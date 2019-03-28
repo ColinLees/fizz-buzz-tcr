1020b49c5e67d557eb0309bd417581ea2935e5e2
David Meek <davidrobertmeek@googlemail.com>
2019-03-28 08:31:12 +0000

    Where I got to

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const fizzOrBuzz = num => {
+    return `${num % 3 === 0 ? 'fizz' : ''}${num % 5 === 0 ? 'buzz' : ''}${num % 3 !== 0 && num % 5 !== 0 ? num : ''}`
+}
+
+const runFizzBuzz = count => {
+    var i = 1
+    var result = []
+
+    while(i <= count){
+
+        runFizzBuzz(i)
+        i++
+    }
+
+}
+
+module.exports = {fizzOrBuzz, runFizzBuzz}
--- index.test.js
+++ index.test.js
@@ -1,8 +1,75 @@
 /* global describe, it */
 const assert = require('chai').assert
+const {fizzOrBuzz} = require('./index')
 
-describe('Testing', () => {
-    it('should work', () => {
-        assert.isTrue(true)
-    })
+describe('fizzOrBuzz', () => {
+
+    const threeTest = (num) => {
+        it('Should return fizz when multiple of 3 and not 5', () => {
+            // Arrange
+    
+            // Act
+            const result = fizzOrBuzz(num)
+    
+            // Assert
+            assert.equal(result, 'fizz')
+        })
+    }
+
+    threeTest(3)
+    threeTest(6)
+    threeTest(9)
+    threeTest(12)
+    threeTest(18)
+    threeTest(21)
+    threeTest(24)
+    threeTest(27)
+    threeTest(33)
+    threeTest(36)
+    
+    const fiveTest = (num) => {
+        it('Should return buzz when multiple of 5 and not 3', () => {
+            // Arrange
+    
+            // Act
+            const result = fizzOrBuzz(num)
+    
+            // Assert
+            assert.equal(result, 'buzz')
+        })
+    }
+
+    fiveTest(5)
+    fiveTest(10)
+    fiveTest(20)
+    fiveTest(25)
+    fiveTest(35)
+    fiveTest(40)
+    fiveTest(50)
+    fiveTest(55)
+    fiveTest(65)
+    fiveTest(70)
+
+    const threeFiveTest = (num) => {
+        it('Should return fizzbuzz when multiple of 5 and 3', () => {
+            // Arrange
+    
+            // Act
+            const result = fizzOrBuzz(num)
+    
+            // Assert
+            assert.equal(result, 'fizzbuzz')
+        })
+    }
+
+    threeFiveTest(15)
+    threeFiveTest(30)
+    threeFiveTest(45)
+    threeFiveTest(60)
+    threeFiveTest(75)
+    threeFiveTest(90)
+    threeFiveTest(105)
+    threeFiveTest(120)
+    threeFiveTest(135)
+    threeFiveTest(150)
 })
